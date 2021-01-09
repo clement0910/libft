a4f4751e3e1682b5bc49f7ee2990991993f2514a
Clement Sapt <csapt@student.42lyon.fr>
2021-01-09 11:17:19 +0100

    fix func print_tab :hammer:

--- srcs/print/print_tab.c
+++ srcs/print/print_tab.c
@@ -6,7 +6,7 @@
 /*   By: csapt <csapt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/29 20:08:36 by csapt             #+#    #+#             */
-/*   Updated: 2020/12/29 20:09:55 by csapt            ###   ########lyon.fr   */
+/*   Updated: 2021/01/09 11:14:13 by csapt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	print_tab(char **tab)
 	int x;
 
 	x = 0;
+	if (!tab)
+		return ;
 	while (tab[x] != NULL)
 	{
 		ft_putendl_fd(tab[x], 1);
