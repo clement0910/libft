03f56b53caed6edb4297ddc2ec1d756e8e3cd1ba
Clément <csapt2001@gmail.com>
2021-04-25 19:39:30 +0200

    add include <stdio.h> :heavy_plus_sign:

--- includes/libft.h
+++ includes/libft.h
@@ -6,13 +6,14 @@
 /*   By: csapt <csapt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/22 20:18:44 by csapt             #+#    #+#             */
-/*   Updated: 2021/04/16 08:45:07 by csapt            ###   ########lyon.fr   */
+/*   Updated: 2021/04/25 19:37:57 by csapt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdio.h>
 # include "libft_conversion.h"
 # include "libft_lst.h"
 # include "libft_gnl.h"
