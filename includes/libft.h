4694d2d815abeb58e9713cc0c9480c4b364a339b
Clément <csapt2001@gmail.com>
2021-04-26 14:42:29 +0200

    add color define to libft.h :art:

--- includes/libft.h
+++ includes/libft.h
@@ -13,6 +13,21 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# define C_N "\033[00m"
+# define C_YB "\033[1;33m"
+# define C_GNB "\033[1;32m"
+# define C_GYB "\033[1;90m"
+# define C_CB "\033[1;36m"
+# define C_RB "\033[1;31m"
+# define C_BB "\033[1;34m"
+# define C_GY "\033[00;90m"
+# define C_B "\033[0;34m"
+# define C_R "\033[0;31m"
+# define C_Y "\033[0;33m"
+# define C_GN "\033[0;32m"
+# define C_P "\033[0;35m"
+# define C_PB "\033[1;35m"
+
 # include <stdio.h>
 # include "libft_conversion.h"
 # include "libft_lst.h"
